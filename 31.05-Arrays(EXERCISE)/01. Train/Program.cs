915ae95e171758a4a13db8918d05626254498c26
Ivaylo Iliev <iliev.ivaylo.p@gmail.com>
2019-06-05 14:24:50 +0300

    update

--- 31.05-Arrays(EXERCISE)/01. Train/Program.cs
+++ 31.05-Arrays(EXERCISE)/01. Train/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _01._Train
 {
@@ -6,7 +7,35 @@ namespace _01._Train
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+                sum += numbers[i];
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+            Console.WriteLine(sum);
+            //int sum = 0;
+
+            //string[] input = new string[n];
+
+            //for (int i = 0; i < input.Length; i++)
+            //{
+            //    input[i] = Console.ReadLine();
+            //}
+
+            //foreach (var number in input)
+            //{
+            //    Console.Write(number + " ");
+            //    int numberConvert = int.Parse(number);
+            //    sum += numberConvert;
+            //}
+            //Console.WriteLine();
+            
+            //Console.WriteLine(sum);
         }
     }
 }
