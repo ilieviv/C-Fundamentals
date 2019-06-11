c2bcf792202fc3989609cf059d3900ec8f91a43c
Ivaylo Iliev <iliev.ivaylo.p@gmail.com>
2019-06-11 11:03:36 +0300

    solution names update

---
+++ [12.2]31.05-Arrays(MORE EXERCISE)/01. Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Encrypt__Sort_and_Print_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stringsNumber = int.Parse(Console.ReadLine());
+            int[] numbers = new int[stringsNumber];
+
+            string vowels = "aAeEiIoOuU";
+            char[] vowelsArr = vowels.ToCharArray();
+            string consonants = "bBcCdDfFgGhHjJkKlLmMnNpPqQrRsStTvVwWxXzZyY";
+            char[] consonantsArr = consonants.ToCharArray();
+
+            for (int i = 0; i < stringsNumber; i++)
+            {
+                string word = Console.ReadLine();
+                char[] letters = word.ToCharArray();
+
+                int sum = 0;
+
+                for (int j = 0; j < letters.Length; j++)
+                {
+                    char currentChar = letters[j];
+
+                    if (vowelsArr.Contains(currentChar))
+                    {
+                        sum += letters[j] * letters.Length;
+                    }
+                    else
+                    {
+                        sum += letters[j] / letters.Length;
+                    }
+                }
+                numbers[i] = sum;
+            }
+
+            Array.Sort(numbers);
+
+            foreach (var element in numbers)
+            {
+                Console.WriteLine(element);
+            }
+
+
+        }
+    }
+}
---
+++ [12.2]31.05-Arrays(MORE EXERCISE)/02. Pascal Triangle/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _02._Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(1);
+
+            if (n == 1)
+            {
+                return;
+            }
+
+            int[] startArr = new int[] { 1, 1 };
+            Console.WriteLine(string.Join(" ", startArr));
+
+            if (n == 2)
+            {
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < startArr.Length + 1; i++)
+                {
+                    int[] arr = new int[startArr.Length + 1];
+                    arr[0] = 1;
+                    arr[arr.Length - 1] = 1;
+
+                    for (int j = 1; j < arr.Length - 1; j++)
+                    {
+                        arr[j] = startArr[j - 1] + startArr[j];
+                    }
+                    Console.WriteLine(string.Join(" ", arr));
+
+                    startArr = arr;
+
+                    if (startArr.Length == n)
+                    {
+                        break;
+                    }
+                }
+            }   
+        }
+    }
+}
