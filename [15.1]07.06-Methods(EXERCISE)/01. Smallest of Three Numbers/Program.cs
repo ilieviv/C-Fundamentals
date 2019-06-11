b3b9a8df878a6717516e94113d0297c1cb4c00c3
Ivaylo Iliev <iliev.ivaylo.p@gmail.com>
2019-06-11 21:56:05 +0300

    methods exercise update

---
+++ [15.1]07.06-Methods(EXERCISE)/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01._Smallest_of_Three_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int result = GetSmallestNumber(firstNumber, secondNumber, thirdNumber);
+            Console.WriteLine(result);
+        }
+
+        static int GetSmallestNumber(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            int minNumber = Math.Min(firstNumber, secondNumber);
+            int finalMinNuber = Math.Min(minNumber, thirdNumber);
+
+            return finalMinNuber;
+        }
+    }
+}
