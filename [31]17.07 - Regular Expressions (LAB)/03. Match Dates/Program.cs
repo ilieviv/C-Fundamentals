d242a84ef2f6c90a3d3afc9a2205ae0f493138e2
Ivaylo Iliev <iliev.ivaylo.p@gmail.com>
2019-07-19 12:39:54 +0300

    regex lab all done

---
+++ [31]17.07 - Regular Expressions (LAB)/03. Match Dates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var regex = new Regex(@"(?<day>\d{2})(?<delimiter>.|-|\/)(?<month>[A-Za-z]+)\2(?<year>\d{4})\b");
+
+            string input = Console.ReadLine();
+
+            MatchCollection matchedDates = regex.Matches(input);
+
+            foreach (Match match in matchedDates)
+            {
+                Console.WriteLine($"Day: {match.Groups["day"]}, Month: {match.Groups["month"]}, Year: {match.Groups["year"]}");
+            }
+        }
+    }
+}
