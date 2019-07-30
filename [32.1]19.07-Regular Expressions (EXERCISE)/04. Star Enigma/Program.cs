51339d34d0b8349baa4e44737a5bcc2e93180bfc
Ivaylo Iliev <iliev.ivaylo.p@gmail.com>
2019-07-30 15:32:23 +0300

    regex exercise all done

---
+++ [32.1]19.07-Regular Expressions (EXERCISE)/04. Star Enigma/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _04._Star_Enigma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Regex regex = new Regex(@"@([A-Z][a-z]+)[^@\-!:>]*:(\d+)[^@\-!:>]*!([A|D])![^@\-!:>]*->(\d+)");
+
+            List<string> attackedPlanets = new List<string>();
+            List<string> destroyedPlanets = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                int key = 0;
+
+                string decryptInput = input.ToLower();
+
+                foreach (char symbol in decryptInput)
+                {
+                    if (symbol == 115 || symbol == 116 || symbol == 97 || symbol == 114)
+                    {
+                        key++;
+                    }
+                }
+
+                string encryptedInput = string.Empty;
+
+                foreach (var symbol in input)
+                {
+                    int newSymbol = symbol - key;
+                    encryptedInput += (char)newSymbol;
+                }
+
+                if (regex.IsMatch(encryptedInput))
+                {
+                    string planetNames = regex.Match(encryptedInput).Groups[1].ToString();
+                    string attackType = regex.Match(encryptedInput).Groups[3].ToString();
+                    
+                    if (attackType == "A")
+                    {
+                        attackedPlanets.Add(planetNames);
+                    }
+
+                    if (attackType == "D")
+                    {
+                        destroyedPlanets.Add(planetNames);
+                    }
+                }
+
+            }
+            attackedPlanets.Sort();
+            destroyedPlanets.Sort();
+
+            Console.WriteLine($"Attacked planets: {attackedPlanets.Count}");
+            foreach (var planet in attackedPlanets)
+            {
+                Console.WriteLine($"-> {planet}");
+            }
+            Console.WriteLine($"Destroyed planets: {destroyedPlanets.Count}");
+            foreach (var planet in destroyedPlanets)
+            {
+                Console.WriteLine($"-> {planet}");
+            }
+        }
+    }
+}
---
+++ [32.1]19.07-Regular Expressions (EXERCISE)/05. Nether Realms/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _05._Nether_Realms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string healthPattern = @"[^0-9+\-*\/.]";
+            Regex healthRegex = new Regex(healthPattern);
+
+            string digitPattern = @"-?\d+\.?\d*";
+            Regex digitRegex = new Regex(digitPattern);
+
+            string operatorPattern = @"[*\/]";
+            Regex operatorRegex = new Regex(operatorPattern);
+
+            string[] demonsNames = Regex.Split(Console.ReadLine(), @"\s*,\s*")
+                .OrderBy(x => x)
+                .ToArray();
+
+            for (int i = 0; i < demonsNames.Length; i++)
+            {
+                string currentDemon = demonsNames[i];
+
+                int currentHealth = 0;
+                MatchCollection healthSymbols = healthRegex.Matches(currentDemon);
+                foreach (Match symbol in healthSymbols)
+                {
+                    currentHealth += char.Parse(symbol.Value);
+                }
+
+                double baseDamage = 0;
+                MatchCollection digitMatch = digitRegex.Matches(currentDemon);
+                foreach (Match number in digitMatch)
+                {
+                    baseDamage += double.Parse(number.Value);
+                }
+
+                MatchCollection operatorMatch = operatorRegex.Matches(currentDemon);
+                foreach (Match operatorr in operatorMatch)
+                {
+                    string @operator = operatorr.Value;
+
+                    if (@operator == "*")
+                    {
+                        baseDamage *= 2;
+                    }
+                    else
+                    {
+                        baseDamage /= 2;
+                    }
+                }
+                Console.WriteLine($"{currentDemon} - {currentHealth} health, {baseDamage:f2} damage");
+            }
+        }
+    }
+}
