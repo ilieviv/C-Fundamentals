1e87742a45451ca13370fa7b7192325f5426bd1d
Ivaylo Iliev <iliev.ivaylo.p@gmail.com>
2019-07-31 23:17:14 +0300

    exam preparation update

--- [32.2]19.07-Regular Expressions(MORE EXERCISE)/1. Winning Ticket/Program.cs
+++ [32.2]19.07-Regular Expressions(MORE EXERCISE)/1. Winning Ticket/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace _1._Winning_Ticket
 {
@@ -6,7 +7,28 @@ namespace _1._Winning_Ticket
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] split = Regex.Split(Console.ReadLine(), @"\s*,\s*");
+
+            Regex winningTicket = new Regex(@"[@]{6,}|[#]{6,}|[$]{6,}|[\^]{6,}");
+            Regex jackpot = new Regex(@"[@]{20}|[#]{20}|[$]{20}|[\^]{20}");
+
+            foreach (var ticket in split)
+            {
+                if (ticket.Length != 20)
+                {
+                    Console.WriteLine("invalid ticket");
+                    continue;
+                }
+
+                if (jackpot.IsMatch(ticket))
+                {
+                    Console.WriteLine($"ticket \"{ticket}\" - ");
+                }
+
+               
+            }
+
+
         }
     }
 }
---
+++ [34]24.07-Exam Preparation/01. Concert/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Concert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var bandsList = new Dictionary<string, List<string>>();
+            var bandsWithTimeList = new Dictionary<string, long>();
+            long totalTime = 0;
+
+            while (input != "start of concert")
+            {
+                string[] inputArr = input.Split("; ");
+                string command = inputArr[0];
+                string bandName = inputArr[1];
+
+                string[] members = inputArr[2].Split(", ");
+
+
+                if (command == "Add")
+                {
+                    if (bandsList.ContainsKey(bandName))
+                    {
+                        foreach (var member in members)
+                        {
+                            if (!bandsList[bandName].Contains(member))
+                            {
+                                bandsList[bandName].Add(member);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        bandsList.Add(bandName, new List<string>());
+
+                        foreach (var member in members)
+                        {
+                            bandsList[bandName].Add(member);
+                        }
+                    }
+                }
+                else if (command == "Play")
+                {
+                    long time = long.Parse(inputArr[2]);
+                    totalTime += time;
+
+                    if (!bandsWithTimeList.ContainsKey(bandName))
+                    {
+                        bandsWithTimeList.Add(bandName, 0);
+                    }
+                    bandsWithTimeList[bandName] += time;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total time: {bandsWithTimeList.Values.Sum()}");
+
+            foreach (var band in bandsWithTimeList.OrderByDescending(kvp => kvp.Value).ThenBy(b => b.Key))
+            {
+                Console.WriteLine($"{band.Key} -> {band.Value}");
+            }
+
+            string bandNameToShow = Console.ReadLine();
+            Console.WriteLine(bandNameToShow);
+            List<string> membersToPrlong = bandsList.FirstOrDefault(b => b.Key == bandNameToShow).Value;
+
+            foreach (var member in membersToPrlong)
+            {
+                Console.WriteLine($"=> {member}");
+            }
+        }
+    }
+}
