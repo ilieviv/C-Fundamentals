25fae6962d98b535d97315cfc2560b4e6e3c015c
Ivaylo Iliev <iliev.ivaylo.p@gmail.com>
2019-08-02 12:35:49 +0300

    demo exam completed

--- [35]26.07-Exam Preparation/Feed the Animals/Program.cs
+++ [35]26.07-Exam Preparation/Feed the Animals/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Feed_the_Animals
 {
@@ -6,7 +8,74 @@ namespace Feed_the_Animals
     {
         static void Main(string[] args)
         {
-            
+            string input = Console.ReadLine();
+
+            Dictionary<string, int> animalDailyLimit = new Dictionary<string, int>();
+            Dictionary<string, List<string>> areaAnimal = new Dictionary<string, List<string>>();
+
+            while (input != "Last Info")
+            {
+                string[] splittedInput = input.Split(":");
+
+                string command = splittedInput[0];
+                string name = splittedInput[1];
+                int food = int.Parse(splittedInput[2]);
+                string area = splittedInput[3];
+
+                if (command == "Add")
+                {
+                    if (!animalDailyLimit.ContainsKey(name))
+                    {
+                        animalDailyLimit.Add(name, food);
+
+                        if (areaAnimal.ContainsKey(area))
+                        {
+                            areaAnimal[area].Add(name);
+                        }
+                        else
+                        {
+                            List<string> n = new List<string>();
+                            n.Add(name);
+                            areaAnimal.Add(area, n);
+                        }
+
+                    }
+                    else
+                    {
+                        animalDailyLimit[name] += food;
+                    }
+                }
+                else
+                {
+                    if (animalDailyLimit.ContainsKey(name))
+                    {
+                        animalDailyLimit[name] -= food;
+
+                        if (animalDailyLimit[name] <= 0)
+                        {
+                            Console.WriteLine($"{name} was successfully fed");
+                            animalDailyLimit.Remove(name);
+                            areaAnimal[area].Remove(name);
+                        }
+                    }
+                }
+
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Animals:");
+
+            foreach (var animal in animalDailyLimit.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{animal.Key} -> {animal.Value}g");
+            }
+
+            Console.WriteLine("Areas with hungry animals:");
+            foreach (var item in areaAnimal.OrderByDescending(x => x.Value.Count).Where(x => x.Value.Count > 0))
+            {
+                Console.WriteLine($"{item.Key} : {item.Value.Count}");
+            }
         }
     }
 }
